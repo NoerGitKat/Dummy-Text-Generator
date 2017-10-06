46b0bb644c0aa652b56085164984aa00c7c4c64b
Noer Paanakker <noer@healthyworkers.nl>
2017-10-06 11:40:03 +0200

    setup webpack config and server

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+	componentDidMount() {
+		console.log('React is running!');
+	}
+
+	render() {
+		return (
+			<div>
+				This app is horrible!!!
+			</div>
+		)
+	}
+}
+
+export default App;
